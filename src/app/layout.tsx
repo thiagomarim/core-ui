376cf5bc4db4865253bdeda7988536e102dacfe7
Thiago <thiago.marim2005@gmail.com>
2024-05-08 13:47:34 -0300

    feat: add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Clean UI",
-  description: "The best UI components for your aplication",
+  description: "The best UI components for your aplication.",
 };
 
 export default function RootLayout({
@@ -13,8 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+    <html lang="en" className="antialiased">
+      <body className={GeistSans.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+
+const NAV_LINKS = [
+  {
+    name: "Docs",
+    href: "/docs",
+  },
+  {
+    name: "Components",
+    href: "/docs/components",
+  },
+];
+
+export default function Header() {
+  const path = usePathname();
+
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border_button/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
+        <Link href="/">
+          <span className="hidden font-bold sm:inline-block text-white">
+            clean/ui
+          </span>
+        </Link>
+        <nav className="flex items-center gap-4 text-sm lg:gap-6">
+          {NAV_LINKS.map((link) => (
+            <Link href={link.href} key={link.name}>
+              <span
+                className={twMerge(
+                  "transition-colors hover:text-primary/80 text-secondary/80",
+                  path === link.href ? "text-primary" : ""
+                )}
+              >
+                {link.name}
+              </span>
+            </Link>
+          ))}
+          <a href="https://github.com/thiagomarim/core-ui" target="_blank">
+            <span className="transition-colors hover:text-primary/80 text-secondary/80">
+              Github
+            </span>
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}

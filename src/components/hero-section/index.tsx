ce0456c47f57a332e834f99e5f36574c8d593a5f
Thiago <thiago.marim2005@gmail.com>
2024-05-08 14:09:36 -0300

    feat: add button UI component

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "../ui/button";
+
 export default function HeroSection() {
   return (
     <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,3 +1,64 @@
-export default function Button() {
-  return <div>Button</div>;
+import * as React from "react";
+import Link from "next/link";
+import { VariantProps, cva } from "class-variance-authority";
+
+import { cn } from "@/components/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-[#262626]",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-[#0F0F0F] text-white border border-[#262626] hover:bg-[#0F0F0F]",
+        secondary:
+          "bg-[#1B1B1B] text-white border border-[#4F4F4F] hover:bg-[#4F4F4F]",
+        outline:
+          "bg-transparent border border-[#262626] hover:bg-[#262626] text-white",
+        icon: "bg-transparent border border-[#262626] hover:bg-[#1B1B1B] text-white",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2 rounded-md",
+        lg: "h-11 px-8 rounded-md",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  href?: string;
 }
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, href, variant, size, ...props }, ref) => {
+    if (href) {
+      return (
+        <Link
+          href={href}
+          className={cn(buttonVariants({ variant, size, className }))}
+        >
+          {children}
+        </Link>
+      );
+    }
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };

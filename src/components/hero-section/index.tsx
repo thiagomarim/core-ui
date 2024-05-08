c225eb877a6305a311b0cb4f0f6a261a2afb64e7
Thiago <thiago.marim2005@gmail.com>
2024-05-08 14:27:11 -0300

    refactor: add buttons into hero section and new title

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -1,16 +1,26 @@
+import Link from "next/link";
 import { Button } from "../ui/button";
 
 export default function HeroSection() {
   return (
-    <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
-      <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1] text-primary">
-        Best minimalist ui components
+    <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-32">
+      <h1 className="text-center text-3xl font-bold leading-tight md:text-5xl lg:leading-[1.1] text-primary">
+        Best minimalist UI components
       </h1>
-      <p className="max-w-[750px] text-center text-lg font-light text-secondary">
-        Beautifully designed components that you can copy and paste into your
-        apps. Accessible. Customizable. Open Source.
+      <p className="max-w-[550px] text-center text-lg font-light text-secondary">
+        Minimalist designed components that you can use in your apps.
+        Accessible. Customizable. Open Source.
       </p>
-      <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10"></div>
+      <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+        <Button>
+          <Link href="/docs">Get Started</Link>
+        </Button>
+        <Button variant={"outline"}>
+          <a href="https://github.com/thiagomarim/core-ui" target="_blank">
+            Github
+          </a>
+        </Button>
+      </div>
     </section>
   );
 }
